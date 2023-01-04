5715f9aa7a2fdc850ecc06450ec13d72eb3802f9
Lokesh <iamlokeshahire@gmail.com>
2023-01-05 01:57:43 +0530

    demo server

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-// const port = 3008;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-
-const PORT = 3008;
-const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = 3008;
+
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use("/api", router);
-server.listen(PORT, () =>
-  console.log(`JSON Server is running on port ${PORT}`)
-);
+server.use(router);
+
+server.listen(port);
+
+// ----------------------------------------------------------------------------------
+
+// const PORT = 3008;
+// const path = require("path");
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, "db.json"));
+// const middlewares = jsonServer.defaults();
+// server.use(middlewares);
+// server.use(jsonServer.bodyParser);
+// server.use("/api", router);
+// server.listen(PORT, () =>
+//   console.log(`JSON Server is running on port ${PORT}`)
+// );
+
+//--------------------------------------------
 
 //frontend x backend
 //5500 x 3000
